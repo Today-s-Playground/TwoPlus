972240d4a32b67b18fd9b6af8a9bf6d52097db2c
hj7321 <oboki2001@naver.com>
2024-05-31 20:29:18 +0900

    폴더 및 파일 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import GlobalStyle from './styles/GlobalStyles';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div>App</div>
+    </>
+  );
 };
 
 export default App;
