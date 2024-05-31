1384453b23ac589b7bf98838e48e80a358ad5683
hj7321 <oboki2001@naver.com>
2024-05-31 20:53:48 +0900

    대표 색상 정의

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -3,6 +3,12 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+  :root {
+  --main-color: #CEACF6;
+  --sub-color: #B341F3;
+  --hover-color: #8D50DB;
+  font-family: "Noto Sans KR", sans-serif;
+  }
 `;
 
 export default GlobalStyle;
