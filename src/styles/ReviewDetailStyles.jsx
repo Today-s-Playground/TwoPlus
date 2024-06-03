24ccd868b3d54e54577ee4e9f5fa26fb7fc46175
hj7321 <oboki2001@naver.com>
2024-06-03 10:29:48 +0900

    style: 리뷰 상세 페이지 스타일 설정

--- src/styles/ReviewDetailStyles.jsx
+++ src/styles/ReviewDetailStyles.jsx
@@ -1,5 +1,30 @@
 import styled from 'styled-components';
 
+export const StCommentBox = styled.div`
+  border: 3px solid coral;
+  min-height: 400px;
+  padding: 10px;
+`;
+
 export const StTextarea = styled.textarea`
   width: 700px;
+  height: 80px;
+  resize: none;
+`;
+
+export const StButton = styled.button`
+  width: 50px;
+  height: 50px;
+  border-radius: 5px;
+`;
+
+export const StUl = styled.ul`
+  border: 3px solid lightblue;
+  padding: 10px;
+`;
+
+export const StLi = styled.li`
+  border: 3px solid lightseagreen;
+  padding: 10px;
+  margin: 10px auto;
 `;
