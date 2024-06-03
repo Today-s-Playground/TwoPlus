18c3a9f5382f92c7f7a6026c07c7f05c895c6b78
hj7321 <oboki2001@naver.com>
2024-06-04 01:09:13 +0900

    feat: fetchInfo() 함수 호출해서 데이터 가져오고, 로딩 상태일 때 css 로더가 나오도록 구현함. supabase 데이터 키값에 맞게 키값 수정함

--- src/pages/communityPage/review/ReviewDetail.jsx
+++ src/pages/communityPage/review/ReviewDetail.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import {
   StBox,
+  StBoxSection,
   StBoxTop,
   StContent,
   StImg,
@@ -10,27 +11,39 @@ import {
   StLine
 } from '../../../styles/CommunityMainStyles';
 import { StButton, StCommentBox, StLi, StTextarea, StUl } from '../../../styles/ReviewDetailStyles';
-
-// 임시 게임 리뷰 데이터
-const data = [
-  { id: 1, gameName: '오버워치', star: 3.5, username: '보라돌이', content: '재밌어요', commentAmount: 3 },
-  { id: 2, gameName: '배틀그라운드', star: 4.0, username: '뚜비', content: '아주 재밌어요', commentAmount: 9 },
-  { id: 3, gameName: '메이플스토리', star: 3.5, username: '나나', content: '정말 재밌어요', commentAmount: 0 },
-  { id: 4, gameName: '또 뭐 있지', star: 3.5, username: '뽀', content: '아무거나', commentAmount: 8 }
-];
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchInfo } from '../../../redux/slices/reviewInfoSlice';
+import './../../../styles/Loading.css';
 
 // 리뷰 상세 페이지
 const ReviewDetail = () => {
   const param = useParams();
-  const filteredData = data.find((obj) => obj.id === parseInt(param.id));
+
+  const dispatch = useDispatch();
+  const { reviewInfo, status, error } = useSelector((state) => state.reviewInfo);
+
+  useEffect(() => {
+    if (status === 'idle') dispatch(fetchInfo());
+  }, [status, dispatch]);
+
+  if (status === 'loading')
+    return (
+      <StBoxSection>
+        <div className="loader"></div>
+      </StBoxSection>
+    );
+  if (status === 'failed') console.log('에러: ', error);
+
+  const filteredData = reviewInfo.find((info) => info.id === parseInt(param.id));
   return (
     <StBox $detail={true}>
       <StBoxTop>
         <StImg src="https://cdn.pixabay.com/photo/2018/03/30/15/11/deer-3275594_1280.jpg" alt="" />
         <StInfo>
-          <p>{filteredData.gameName}</p>
-          <p>⭐️{filteredData.star}</p>
-          <p>{filteredData.username}</p>
+          <p>{filteredData.game_name}</p>
+          <p>⭐️{filteredData.star_score}</p>
+          <p>{filteredData.user_name}</p>
         </StInfo>
         <StLikedBox $detail={true}>
           {/* <p>(좋아요 아이콘)</p> */}
@@ -40,7 +53,7 @@ const ReviewDetail = () => {
       </StBoxTop>
       <StContent $detail={true}>{filteredData.content}</StContent>
       <StLine>
-        <p>{filteredData.commentAmount}</p>
+        <p>(댓글 개수)</p>
         <p>(댓글 아이콘)</p>
       </StLine>
       <StCommentBox>
