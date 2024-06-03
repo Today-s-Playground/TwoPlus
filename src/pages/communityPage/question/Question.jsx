b2af350c090ed6e6f0026e49de06c4a13f463fb0
hj7321 <oboki2001@naver.com>
2024-06-03 16:22:41 +0900

    feat: 게임 질문 페이지 구현

--- src/pages/communityPage/question/Question.jsx
+++ src/pages/communityPage/question/Question.jsx
@@ -1,6 +1,43 @@
-// 질문 페이지
+import StrategyFormat from '../../../components/community/StrategyFormat';
+
+// 임시 게임 질문 데이터
+const questionData = [
+  {
+    id: 1,
+    gameName: '게임이름1',
+    title: '게시글제목1',
+    content: '질문내용1',
+    username: '유저1',
+    commentAmount: 10
+  },
+  {
+    id: 2,
+    gameName: '게임이름2',
+    title: '게시글제목2',
+    content: '질문내용2',
+    username: '유저2',
+    commentAmount: 20
+  },
+  {
+    id: 3,
+    gameName: '게임이름3',
+    title: '게시글제목3',
+    content: '질문내용3',
+    username: '유저3',
+    commentAmount: 30
+  },
+  {
+    id: 4,
+    gameName: '게임이름4',
+    title: '게시글제목4',
+    content: '질문내용4',
+    username: '유저4',
+    commentAmount: 40
+  }
+];
+
 const Question = () => {
-  return <div>질문 페이지</div>;
+  return <StrategyFormat data={questionData} />;
 };
 
 export default Question;
