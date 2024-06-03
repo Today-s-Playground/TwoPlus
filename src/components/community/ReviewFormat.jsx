c1a823ccfa6eb58b94b84c857f7e72fc73853349
hj7321 <oboki2001@naver.com>
2024-06-04 01:10:49 +0900

    feat: fetchInfo() 함수 호출해서 데이터 가져오고, 로딩 상태일 때 css 로더가 나오도록 구현함

--- src/components/community/ReviewFormat.jsx
+++ src/components/community/ReviewFormat.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
   StBox,
@@ -11,20 +11,39 @@ import {
   StLikedBox,
   StLine
 } from './../../styles/CommunityMainStyles';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchInfo } from '../../redux/slices/reviewInfoSlice';
+import './../../styles/Loading.css';
 
-const ReviewFormat = ({ data }) => {
+const ReviewFormat = ({ isSliced }) => {
   const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+  const { reviewInfo, status, error } = useSelector((state) => state.reviewInfo);
+
+  useEffect(() => {
+    if (status === 'idle') dispatch(fetchInfo());
+  }, [status, dispatch]);
+
+  if (status === 'loading')
+    return (
+      <StBoxSection>
+        <div className="loader"></div>
+      </StBoxSection>
+    );
+  if (status === 'failed') console.log('에러: ', error);
+  const data = isSliced ? reviewInfo.slice(0, 2) : reviewInfo;
 
   return (
     <StBoxSection>
-      {data.map((obj) => (
-        <StBox key={obj.id} onClick={() => navigate(`/review/${obj.id}`)}>
+      {data.map((info) => (
+        <StBox key={info.id} onClick={() => navigate(`/review/${info.id}`)}>
           <StBoxTop>
             <StImg src="https://cdn.pixabay.com/photo/2018/03/30/15/11/deer-3275594_1280.jpg" alt="" />
             <StInfo>
-              <p>{obj.gameName}</p>
-              <p>⭐️{obj.star}</p>
-              <p>{obj.username}</p>
+              <p>{info.game_name}</p>
+              <p>⭐️{info.star_score}</p>
+              <p>{info.user_name}</p>
             </StInfo>
             <StLikedBox>
               {/* <p>(좋아요 아이콘)</p> */}
@@ -32,9 +51,9 @@ const ReviewFormat = ({ data }) => {
               <p>66</p>
             </StLikedBox>
           </StBoxTop>
-          <StContent>{obj.content}</StContent>
+          <StContent>{info.content}</StContent>
           <StLine>
-            <p>{obj.commentAmount}</p>
+            <p>(댓글 개수)</p>
             <p>(댓글 아이콘)</p>
           </StLine>
         </StBox>
