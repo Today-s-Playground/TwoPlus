11a6db471a7de47affd91597f5869ec8237262fa
hj7321 <oboki2001@naver.com>
2024-06-04 01:12:35 +0900

    CommunityMain 컴포넌트에서 임포트한 경로 수정 및 필요 없는 폴더, 파일 삭제

--- src/pages/communityPage/CommunityMain.jsx
+++ src/pages/communityPage/CommunityMain.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import GameReview from '../../components/community/GameReview';
-import GameStrategy from './../../components/community/GameStrategy';
+import GameStrategy from '../../components/community/GameStrategy';
 import GameQuestion from '../../components/community/GameQuestion';
 
 const CommunityMain = () => {
--- src/pages/communityPage/recruit/Recruit.jsx
+++
@@ -1,6 +0,0 @@
-// 동료 모집 페이지
-const Recruit = () => {
-  return <div>동료 모집 페이지</div>;
-};
-
-export default Recruit;
