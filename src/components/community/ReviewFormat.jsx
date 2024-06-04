655d1adf1bc131872ace424e1e2ab5803a81a9d6
hj7321 <oboki2001@naver.com>
2024-06-04 20:48:21 +0900

    chore: 커스텀 훅을 호출하는 것으로 수정

--- src/components/community/ReviewFormat.jsx
+++ src/components/community/ReviewFormat.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
   StBox,
@@ -9,55 +8,50 @@ import {
   StLiked,
   StLikedBox,
   StLine,
-  StBoxSection
+  StBoxSection,
+  StCommentBox
 } from './../../styles/CommunityMainStyles';
-import { useDispatch, useSelector } from 'react-redux';
 import { fetchInfo } from '../../redux/slices/reviewInfoSlice';
 import './../../styles/Loading.css';
+import useFetch from '../../hooks/useFetch';
 
-const ReviewFormat = ({ isSliced, $notMain }) => {
+const ReviewFormat = ({ isSliced, $isMain }) => {
   const navigate = useNavigate();
-
-  const dispatch = useDispatch();
-  const { reviewInfo, status, error } = useSelector((state) => state.reviewInfo);
-
-  useEffect(() => {
-    if (status === 'idle') dispatch(fetchInfo());
-  }, [status, dispatch]);
-
-  if (status === 'loading')
-    return (
-      <StBoxSection $notMain={$notMain}>
-        <div className="loader"></div>
-      </StBoxSection>
-    );
-  if (status === 'failed') console.log('에러: ', error);
-  const data = isSliced ? reviewInfo.slice(0, 2) : reviewInfo;
+  const data = useFetch('reviewInfo', fetchInfo, isSliced);
 
   return (
-    <StBoxSection $notMain={$notMain}>
-      {data.map((info) => (
-        <StBox key={info.id} onClick={() => navigate(`/review/${info.id}`)}>
-          <StBoxTop>
-            <StImg src="https://cdn.pixabay.com/photo/2018/03/30/15/11/deer-3275594_1280.jpg" alt="" />
-            <StInfo>
-              <p>{info.game_name}</p>
-              <p>⭐️{info.star_score}</p>
-              <p>{info.user_name}</p>
-            </StInfo>
-            <StLikedBox>
-              {/* <p>(좋아요 아이콘)</p> */}
-              <StLiked src="../../../../src/images/liked.png" alt="" />
-              <p>{info.liked_amount}</p>
-            </StLikedBox>
-          </StBoxTop>
-          <StContent>{info.content}</StContent>
-          <StLine>
-            <p>(댓글 개수)</p>
-            <p>(댓글 아이콘)</p>
-          </StLine>
-        </StBox>
-      ))}
+    <StBoxSection $isMain={$isMain}>
+      {data.length === 0 ? (
+        <div className="loader"></div>
+      ) : (
+        data.map((info) => (
+          <StBox key={info.id} onClick={() => navigate(`/review/${info.id}`)}>
+            <StBoxTop>
+              <StImg src="https://cdn.pixabay.com/photo/2018/03/30/15/11/deer-3275594_1280.jpg" alt="" />
+              <StInfo>
+                <p>{info.game_name}</p>
+                <p>⭐️{info.star_score}</p>
+                <p>{info.user_name}</p>
+              </StInfo>
+              <StLikedBox>
+                {/* <p>(좋아요 아이콘)</p> */}
+                <StLiked src="../../../../src/images/liked.png" alt="" />
+                <p>{info.liked_amount}</p>
+              </StLikedBox>
+            </StBoxTop>
+            <StContent>{info.content}</StContent>
+            <StLine>
+              <div>
+                <p>{info.created_at.split('T')[0]}</p>
+              </div>
+              <StCommentBox>
+                <p>(댓글 개수)</p>
+                <p>(댓글 아이콘)</p>
+              </StCommentBox>
+            </StLine>
+          </StBox>
+        ))
+      )}
     </StBoxSection>
   );
 };
--- src/components/community/StrategyFormat.jsx
+++ src/components/community/StrategyFormat.jsx
@@ -1,46 +1,41 @@
 import { useNavigate } from 'react-router-dom';
 import { StBox2, StBoxSection, StImg, StContent, StBoxBottom, StComment } from './../../styles/CommunityMainStyles';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchInfo } from '../../redux/slices/strategyInfoSlice';
 import './../../styles/Loading.css';
+import useFetch from '../../hooks/useFetch';
+import { fetchStrategyInfo } from './../../redux/slices/strategyInfoSlice';
+import { fetchQuestionInfo } from './../../redux/slices/questionInfoSlice';
 
-const StrategyFormat = ({ isSliced, path, $detail, $notMain }) => {
+const StrategyFormat = ({ isSliced, path, $detail, $isMain }) => {
   const navigate = useNavigate();
 
-  const dispatch = useDispatch();
-  const { strategyInfo, status, error } = useSelector((state) => state.strategyInfo);
-
-  useEffect(() => {
-    if (status === 'idle') dispatch(fetchInfo());
-  }, [status, dispatch]);
-
-  if (status === 'loading')
-    return (
-      <StBoxSection $notMain={$notMain}>
-        <div className="loader"></div>
-      </StBoxSection>
-    );
-  if (status === 'failed') console.log('에러: ', error);
-  const data = isSliced ? strategyInfo.slice(0, 3) : strategyInfo;
+  const data = useFetch(
+    path === 'strategy' ? 'strategyInfo' : 'questionInfo',
+    path === 'strategy' ? fetchStrategyInfo : fetchQuestionInfo,
+    isSliced
+  );
 
   return (
-    <StBoxSection $notMain={$notMain}>
-      {data.map((info) => (
-        <StBox2 key={info.id} onClick={() => navigate(`/${path}/${info.id}`)}>
-          <StImg src="https://cdn.pixabay.com/photo/2018/03/30/15/11/deer-3275594_1280.jpg" alt="" />
-          <p>{info.game_name}</p>
-          <p>{info.title}</p>
-          <StContent $detail={$detail}>{info.content}</StContent>
-          <StBoxBottom>
+    <StBoxSection $isMain={$isMain}>
+      {data.length === 0 ? (
+        <div className="loader"></div>
+      ) : (
+        data.map((info) => (
+          <StBox2 key={info.id} onClick={() => navigate(`/${path}/${info.id}`)}>
+            <StImg src="https://cdn.pixabay.com/photo/2018/03/30/15/11/deer-3275594_1280.jpg" alt="" />
+            <p>{info.game_name}</p>
+            <p>{info.title}</p>
             <p>{info.user_name}</p>
-            <StComment>
-              <p>{info.comment_amount}</p>
-              <p>(댓글 아이콘)</p>
-            </StComment>
-          </StBoxBottom>
-        </StBox2>
-      ))}
+            <StContent $detail={$detail}>{info.content}</StContent>
+            <StBoxBottom>
+              <p>{info.created_at.split('T')[0]}</p>
+              <StComment>
+                <p>{info.comment_amount}</p>
+                <p>(댓글 아이콘)</p>
+              </StComment>
+            </StBoxBottom>
+          </StBox2>
+        ))
+      )}
     </StBoxSection>
   );
 };
