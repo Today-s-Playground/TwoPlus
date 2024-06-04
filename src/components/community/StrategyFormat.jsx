7ddc114a544a7b7a9175fb7e8fa691bd613a941d
hj7321 <oboki2001@naver.com>
2024-06-04 15:50:40 +0900

    feat: supabase의 Strategy 테이블 데이터 연결

--- src/components/community/StrategyFormat.jsx
+++ src/components/community/StrategyFormat.jsx
@@ -1,21 +1,41 @@
 import { useNavigate } from 'react-router-dom';
 import { StBox2, StBoxSection, StImg, StContent, StBoxBottom, StComment } from './../../styles/CommunityMainStyles';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchInfo } from '../../redux/slices/strategyInfoSlice';
+import './../../styles/Loading.css';
 
-const StrategyFormat = ({ data, path }) => {
+const StrategyFormat = ({ isSliced, path, $detail, $notMain }) => {
   const navigate = useNavigate();
 
+  const dispatch = useDispatch();
+  const { strategyInfo, status, error } = useSelector((state) => state.strategyInfo);
+
+  useEffect(() => {
+    if (status === 'idle') dispatch(fetchInfo());
+  }, [status, dispatch]);
+
+  if (status === 'loading')
+    return (
+      <StBoxSection $notMain={$notMain}>
+        <div className="loader"></div>
+      </StBoxSection>
+    );
+  if (status === 'failed') console.log('에러: ', error);
+  const data = isSliced ? strategyInfo.slice(0, 3) : strategyInfo;
+
   return (
-    <StBoxSection>
-      {data.map((obj) => (
-        <StBox2 key={obj.id} onClick={() => navigate(`/${path}/${obj.id}`)}>
+    <StBoxSection $notMain={$notMain}>
+      {data.map((info) => (
+        <StBox2 key={info.id} onClick={() => navigate(`/${path}/${info.id}`)}>
           <StImg src="https://cdn.pixabay.com/photo/2018/03/30/15/11/deer-3275594_1280.jpg" alt="" />
-          <p>{obj.gameName}</p>
-          <p>{obj.title}</p>
-          <StContent>{obj.content}</StContent>
+          <p>{info.game_name}</p>
+          <p>{info.title}</p>
+          <StContent $detail={$detail}>{info.content}</StContent>
           <StBoxBottom>
-            <p>{obj.username}</p>
+            <p>{info.user_name}</p>
             <StComment>
-              <p>{obj.commentAmount}</p>
+              <p>{info.comment_amount}</p>
               <p>(댓글 아이콘)</p>
             </StComment>
           </StBoxBottom>
