d1594155f704604149a6671bbcfe6d992d534939
seongbin1013 <plok82890@gmail.com>
2024-06-04 20:38:58 +0900

    chore: 마이페이지 코드 수정

--- src/pages/myPage/MyMain.jsx
+++ src/pages/myPage/MyMain.jsx
@@ -1,92 +1,63 @@
-import styled from 'styled-components';
-
-const Main = styled.main`
-  margin-top: 30px;
-  width: 100%;
-`;
-
-const ProfileSection = styled.section`
-  padding: 100px;
-  border-radius: 0 0 50px 50px;
-  background-color: #7c63fc;
-  height: auto;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const UserInfo = styled.section`
-  .userName {
-    display: flex;
-    align-items: center;
-  }
-
-  .nickName {
-    font-size: 50px;
-    margin: 0 15px 20px 0px;
-  }
-  .userInfor {
-    font-size: 20px;
-  }
-`;
-
-const EmailInput = styled.input`
-  width: 200px;
-  height: 30px;
-  border-radius: 50px;
-  text-align: center;
-  border: none;
-  outline: none;
-`;
-
-const Profile = styled.section`
-  width: 130px;
-  height: 130px;
-  border-radius: 50%;
-  background-color: yellow;
-`;
-
-const GameSection = styled.section`
-  background-color: green;
-  padding: 100px;
-`;
-const GameDetails = styled.div``;
-
-const GameInfo = styled.div``;
-
-const InforSection = styled.section``;
-
-const ListSection = styled.section``;
+import {
+  EmailInput,
+  GameSection,
+  InforSection,
+  InforState,
+  ListSection,
+  Main,
+  ProfileSection,
+  UserInfo
+} from '../../styles/MyMainStyles';
+import InfoCard from './InfoCard';
 
 const MyMain = () => {
   return (
     <Main>
-      <ProfileSection>
-        <UserInfo>
+      <ProfileSection />
+      <UserInfo>
+        <div className="userBox">
           <div className="userName">
             <div className="nickName">닉네임</div>
             <p className="userInfor">실명, 🇰🇷 korea, Republic of</p>
           </div>
           <EmailInput placeholder="이메일을 입력하세요 >" />
-        </UserInfo>
-        <Profile />
-      </ProfileSection>
+        </div>
+        <div className="profileBox">
+          <img src="https://ifh.cc/g/dgyJCA.png" alt="이미지" className="profilePic" />
+          <div className="correctionBox">
+            <img src="https://ifh.cc/g/4P9vHm.png" alt="수정이미지" className="correction" />
+          </div>
+        </div>
+      </UserInfo>
       {/* 가장 많이 플레이한 게임 */}
       <GameSection>
-        <GameDetails>
-          <GameInfo>
-            <p>asdasd</p>
-          </GameInfo>
-        </GameDetails>
+        <div className="gamePic">
+          <img src="https://ifh.cc/g/mJwCpw.png" alt="game-pic" />
+        </div>
+        <div className="gameInfo">
+          <div>
+            <p className="game">가장 많이 플레이어 한 게임 😮</p>
+            <p className="gameName">PUBG: BATTLEGROUNDS</p>
+          </div>
+          <p className="gameTime">789시간</p>
+          <p className="comment">그만 좀 하세요</p>
+        </div>
+        <div className="expression">
+          <div>👍🏻</div>
+          <div>👎🏻</div>
+          <div>💬</div>
+        </div>
       </GameSection>
       {/* 나의 정보 */}
       <InforSection>
-        <div>찜한 게임 목록</div>
-        <div>내가 쓴 글</div>
-        <div>친구 목록</div>
+        <InfoCard title={'찜한 게임 목록'} />
+        <InfoCard title={'내가 쓴 글'} />
+        <InfoCard title={'내가 쓴 댓글'} />
       </InforSection>
       {/* 구매 목록 */}
       <ListSection>
-        <div>구매목록</div>
+        <div className="purchaseList">구매목록 ➡️</div>
+        <img src="https://ifh.cc/g/kNDdqB.jpg" alt="" />
       </ListSection>
     </Main>
   );
---
+++ src/styles/MyMainStyles.jsx
@@ -0,0 +1,170 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+  margin-top: 30px;
+  width: 100%;
+`;
+
+export const ProfileSection = styled.div`
+  border-radius: 0 0 50px 50px;
+  background-color: #7c63fc;
+  height: 800px;
+  position: absolute;
+  top: 100px;
+  width: 100%;
+  z-index: -1;
+`;
+
+export const UserInfo = styled.section`
+  padding: 50px;
+  margin: 50px;
+  display: flex;
+  justify-content: space-between;
+
+  .userName {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+
+  .nickName {
+    font-size: 50px;
+    margin: 10px 0px 20px 0px;
+  }
+  .userInfor {
+    font-size: 20px;
+  }
+
+  .profileBox {
+    position: relative;
+
+    .profilePic {
+      width: 150px;
+      height: 150px;
+      border-radius: 50%;
+      background-color: #fcfc76;
+    }
+
+    .correctionBox {
+      border-radius: 50%;
+      position: absolute;
+      width: 50px;
+      height: 50px;
+      background-color: white;
+      bottom: 0px;
+      right: 0px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .correction {
+      width: 30px;
+      height: 30px;
+    }
+  }
+`;
+
+export const EmailInput = styled.input`
+  width: 250px;
+  height: 45px;
+  border-radius: 50px;
+  text-align: center;
+  border: none;
+  outline: none;
+`;
+
+export const GameSection = styled.section`
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 3px 3px 0px;
+  margin: 50px;
+  padding: 50px;
+  display: flex;
+  gap: 250px;
+  text-align: center;
+
+  .gameInfo {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  .gamePic {
+    img {
+      height: 100%;
+    }
+  }
+
+  .game {
+    font-size: 25px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  .gameName {
+    font-size: 18px;
+    color: #666;
+  }
+
+  .gameTime {
+    font-size: 40px;
+    font-weight: 300;
+    color: #999;
+  }
+
+  .comment {
+    font-size: 20px;
+    color: #333;
+  }
+
+  .expression {
+    display: flex;
+    gap: 50px;
+    font-size: 2rem;
+    align-items: flex-end;
+  }
+`;
+
+export const InforSection = styled.section`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+`;
+
+export const InforState = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  width: 300px;
+  height: 150px;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 3px 3px 0px;
+  margin: 10px;
+
+  .title {
+    font-size: 25px;
+    margin-bottom: 25px;
+  }
+  .number {
+    font-size: 50px;
+    font-weight: bold;
+  }
+`;
+
+export const ListSection = styled.section`
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 3px 3px 0px;
+  margin: 50px;
+  padding: 50px;
+  text-align: center;
+
+  .purchaseList {
+    font-size: 30px;
+    margin-bottom: 40px;
+  }
+  img {
+    width: 170px;
+    height: 170px;
+  }
+`;
