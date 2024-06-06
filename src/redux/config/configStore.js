155b14a11ba4d396da5c25b28d6cf684c5798a0c
hj7321 <oboki2001@naver.com>
2024-06-06 17:29:27 +0900

    feat: configureStore에 새로 만든 slice들 추가

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -2,12 +2,18 @@ import { configureStore } from '@reduxjs/toolkit';
 import reviewInfoSlice from '../slices/reviewInfoSlice';
 import strategyInfoSlice from '../slices/strategyInfoSlice';
 import questionInfoSlice from '../slices/questionInfoSlice';
+import reviewCommentSlice from '../slices/reviewCommentSlice';
+import strategyCommentSlice from '../slices/strategyCommentSlice';
+import questionCommentSlice from '../slices/questionCommentSlice';
 
 const store = configureStore({
   reducer: {
     reviewInfo: reviewInfoSlice,
     strategyInfo: strategyInfoSlice,
-    questionInfo: questionInfoSlice
+    questionInfo: questionInfoSlice,
+    reviewComment: reviewCommentSlice,
+    strategyComment: strategyCommentSlice,
+    questionComment: questionCommentSlice
   }
 });
 
