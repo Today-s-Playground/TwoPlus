36fe96ca1572c0797485b28ae1b125bf3d1ec8be
hj7321 <oboki2001@naver.com>
2024-06-06 17:27:52 +0900

    컴포넌트 이름을 CommunityLayout으로 바꿈

--- src/pages/communityPage/CommunityMain.jsx
+++ src/pages/communityPage/CommunityMain.jsx
@@ -1,15 +1,15 @@
 import GameReview from '../../components/community/GameReview';
 import GameStrategy from '../../components/community/GameStrategy';
 import GameQuestion from '../../components/community/GameQuestion';
-import CommunitySideBar from '../../shared/CommunityLayout';
+import CommunityLayout from '../../shared/CommunityLayout';
 
 const CommunityMain = () => {
   return (
-    <CommunitySideBar>
+    <CommunityLayout>
       <GameReview />
       <GameStrategy />
       <GameQuestion />
-    </CommunitySideBar>
+    </CommunityLayout>
   );
 };
 
